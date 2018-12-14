943b647e211e72cada9c37de0b26706ee76160e0
SophieHuan <45580316+SophieHuan@users.noreply.github.com>
2018-12-14 10:33:24 +0800

    Add files via upload

--- PolyA_seq/Analysis/Step2.Trimming_polyA_tail.pl
+++ PolyA_seq/Analysis/Step2.Trimming_polyA_tail.pl
@@ -16,25 +16,25 @@ my $file="Col_1_end1_raw_q20p50Q33_l16aTGGAATTCTCGGG_adapter_del3.fq";
 #FFJJJJJJJJJJJJJJJJJ-FJJJFJJJJJ<FJJJJJJJJFJJJJJJFAFJJJFJJJJJJJJJA-F<AAJJFAJJJJJJJJJF<7F<-F-<<<-7FJ<JFJ<-FA<7
 
 
-# This defines the seperator of each read in .fastq document
+# Define the seperator of each read in .fastq document
 $/="\@ST-";
 
 open (IN,$file)||die;
-open (OUT1,">".$file."_delA.fq")||die;
-open (OUT2,">".$file."_polyA_length.txt")||die;
+open (OUT1,">".$file."_delA_v2.fq")||die;
+open (OUT2,">".$file."_polyA_length_v2.txt")||die;
 while (my $line=<IN>) {
 	chomp $line;
 	$line=~s/\n$// if ($line=~/\n$/);
 	next if ($line eq "");
 	my @lines=split(/\n/,$line);
 
-	# This defines the poly(A) tail as constitutive A¡¯s at the 3¡ä-end of a read, allowing up to 5 interspersed G bases 
-	if ($lines[1]=~/(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)$/) {
+	# Define the poly(A) tail as constitutive A¡¯s at the 3¡ä-end of a read, allowing up to 5 interspersed G bases 
+	if ($lines[1]=~/(AAAA)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)$/) {
 
-		# This calculates the length of poly(A) tail
-		my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)+length($9)+length($10);
-
-		# Trimmed reads with shorter than 18 nt in length are removed
+		# Calculate the length of poly(A) tail
+		my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)+length($9)+length($10)+length($11);
+		
+		# Trimmed reads with shorter than 18 nt in length were removed
 		my @lables=split(" ",$lines[0]);
 		if (length($lines[1])>= ($len+18)) {
 
@@ -42,47 +42,54 @@ while (my $line=<IN>) {
 			print OUT1 "\@ST-$lines[0]\n".substr($lines[1],0,(length($lines[1])-$len))."\n$lines[2]\n".substr($lines[3],0,(length($lines[1])-$len))."\n";
 
 			#print reads information with poly(A) tail length
-			print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8.$9.$10."\n";
+			my $quality=substr($lines[3],(length($lines[1])-$len),$len);
+			print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8.$9.$10.$11."\t".$quality."\n";
 		}
-	}
-	elsif ($lines[1]=~/(A+)(A|T|C|G)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|G)$/) {
-		my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)+length($9)+length($10);
+	}	
+	elsif ($lines[1]=~/(AAAA)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|G)$/) {
+		my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)+length($9)+length($10)+length($11);
 		my @lables=split(" ",$lines[0]);
 		if (length($lines[1])>= ($len+18)) {
 			print OUT1 "\@ST-$lines[0]\n".substr($lines[1],0,(length($lines[1])-$len))."\n$lines[2]\n".substr($lines[3],0,(length($lines[1])-$len))."\n";
-			print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8.$9.$10."\n";
+			my $quality=substr($lines[3],(length($lines[1])-$len),$len);
+			print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8.$9.$10.$11."\t".$quality."\n";
 		}
 	}
-	elsif ($lines[1]=~/(A+)(A|G)(A+)(A|T|C|G)(A+)(A|T|C|G)(A+)(A|G)$/) {
-			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8);
+	elsif ($lines[1]=~/(AAAA)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|T|C|G)(A+)(A|G)$/) {
+			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)+length($9);
 			my @lables=split(" ",$lines[0]);
 			if (length($lines[1])>= ($len+18)) {
 				print OUT1 "\@ST-$lines[0]\n".substr($lines[1],0,(length($lines[1])-$len))."\n$lines[2]\n".substr($lines[3],0,(length($lines[1])-$len))."\n";
-				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8."\n";
+				my $quality=substr($lines[3],(length($lines[1])-$len),$len);
+				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8.$9."\t".$quality."\n";
 		}
 	}
-	elsif ($lines[1]=~/(A+)(A|T|C|G)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)$/) {
-			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8);
+	elsif ($lines[1]=~/(AAAA)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|T|C|G)(A+)(A|G)$/) {
+			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)+length($9);
 			my @lables=split(" ",$lines[0]);
 			if (length($lines[1])>= ($len+18)) {
 				print OUT1 "\@ST-$lines[0]\n".substr($lines[1],0,(length($lines[1])-$len))."\n$lines[2]\n".substr($lines[3],0,(length($lines[1])-$len))."\n";
-				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8."\n";
+				my $quality=substr($lines[3],(length($lines[1])-$len),$len);
+				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8.$9."\t".$quality."\n";
 		}
 	}
-	elsif ($lines[1]=~/(A+)(A|T|C|G)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|G)$/) {
-			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8);
+	elsif ($lines[1]=~/(AAAA)(A+)(A|T|C|G)(A+)(A|T|C|G)(A+)(A|G)(A+)(A|G)$/) {
+			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7)+length($8)+length($9);
 			my @lables=split(" ",$lines[0]);
 			if (length($lines[1])>= ($len+18)) {
 				print OUT1 "\@ST-$lines[0]\n".substr($lines[1],0,(length($lines[1])-$len))."\n$lines[2]\n".substr($lines[3],0,(length($lines[1])-$len))."\n";
-				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8."\n";
+				my $quality=substr($lines[3],(length($lines[1])-$len),$len);
+				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7.$8.$9."\t".$quality."\n";
 		}
 	}
-	elsif ($lines[1]=~/(A+)(A|T|C|G)(A+)(A|T|C|G)(A+)(A|G)$/){
-			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6);
+
+	elsif ($lines[1]=~/(AAAA)(A+)(A|T|C|G)(A+)(A|T|C|G)(A+)(A|G)$/){
+			my $len=length($1)+length($2)+length($3)+length($4)+length($5)+length($6)+length($7);
 			my @lables=split(" ",$lines[0]);
 			if (length($lines[1])>= ($len+18)) {
 				print OUT1 "\@ST-$lines[0]\n".substr($lines[1],0,(length($lines[1])-$len))."\n$lines[2]\n".substr($lines[3],0,(length($lines[1])-$len))."\n";
-				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6."\n";
+				my $quality=substr($lines[3],(length($lines[1])-$len),$len);
+				print OUT2 "ST-$lables[0]\t".$len."\t".$1.$2.$3.$4.$5.$6.$7."\t".$quality."\n";
 		}
 	}
 }
